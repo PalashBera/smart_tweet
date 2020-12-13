306aad1255e90185f368e28ecd6e20d800b2f2b3
Palash Bera <palashbera1234@gmail.com>
2020-12-13 16:10:56 +0530

    Adding comment and retweet icon link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @tweets = Tweet.all.includes(:user).decending
+    @tweets = Tweet.all.includes(:user, :comments).decending
   end
 end
